12e0550d08f8bf77e773532ca8e9c0d00eb26619
Dmitrii Kilishek <mentalclear@gmail.com>
2022-04-14 09:35:27 -0400

    Removed my first solution

--- multsthreefive/mults.go
+++ multsthreefive/mults.go
@@ -1,19 +1,5 @@
 package multsthreefive
 
-// func Multiple3And5(number int) (sum int) {
-// 	for i := 1; i < number; i++ {
-// 		if i%3 == 0 {
-// 			sum += i
-// 		} else if i%5 == 0 {
-// 			sum += i
-// 		} else if i%3 == 0 && i%5 == 0 {
-// 			continue
-// 		}
-// 	}
-
-// 	return
-// }
-
 // Actually this gives 0 for negatives as well
 
 func Multiple3And5(number int) (sum int) {
