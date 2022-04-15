bd57479d844222be2784eb1fabc56a6ad3840ac1
Dmitrii Kilishek <mentalclear@gmail.com>
2022-04-15 09:12:06 -0400

    Update final solution

--- spinwords/spinwords.go
+++ spinwords/spinwords.go
@@ -1,5 +1,27 @@
 package spinwords
 
+import (
+	"strings"
+)
+
 func SpinWords(str string) string {
-	return "pizza"
-} // SpinWords
+	var result string
+	words := strings.Fields(str)
+	for _, word := range words {
+		if len(word) > 4 {
+			result += rotateWord(word) + " "
+		} else {
+			result += word + " "
+		}
+	}
+	return strings.TrimSpace(result)
+}
+
+func rotateWord(word string) string {
+	rune_arr := []rune(word)
+	var rev []rune
+	for i := len(rune_arr) - 1; i >= 0; i-- {
+		rev = append(rev, rune_arr[i])
+	}
+	return string(rev)
+}
