bbf89473b7ba3e064543cb1ae2fdadfdf0325aca
Dmitrii Kilishek <mentalclear@gmail.com>
2022-04-19 13:04:43 -0400

    Added another solution using library

--- ipaddr/ipaddr.go
+++ ipaddr/ipaddr.go
@@ -1,12 +1,22 @@
 package ipaddr
 
-import "regexp"
+import "net"
+
+// func Is_valid_ip(ip string) bool {
+// 	result, err := regexp.MatchString(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`, ip)
+// 	if err == nil {
+// 		return result
+// 	}
+
+// 	return false
+// }
+
+// This one is using net library.
 
 func Is_valid_ip(ip string) bool {
-	result, err := regexp.MatchString(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`, ip)
-	if err == nil {
-		return result
+	res := net.ParseIP(ip)
+	if res == nil {
+		return false
 	}
-
-	return false
+	return true
 }
