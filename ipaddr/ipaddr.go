7c54c8bfcc4a700d1ab2cb7b05c3b2735da8cbd0
Dmitrii Kilishek <mentalclear@gmail.com>
2022-04-19 13:00:14 -0400

    Fixed IPaddress kata

--- ipaddr/ipaddr.go
+++ ipaddr/ipaddr.go
@@ -2,10 +2,8 @@ package ipaddr
 
 import "regexp"
 
-// TODO Fix the regex!
-
 func Is_valid_ip(ip string) bool {
-	result, err := regexp.MatchString(`^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$`, ip)
+	result, err := regexp.MatchString(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`, ip)
 	if err == nil {
 		return result
 	}
